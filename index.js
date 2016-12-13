3d6a1a8a594ab222ffa2896e7a9722119231eff5
Peter Širka <petersirka@gmail.com>
2016-12-13 08:23:32 +0100

    Update insert() and upsert().

--- index.js
+++ index.js
@@ -21,7 +21,7 @@
 
 /**
  * @module NoSQL
- * @version 2.3.0
+ * @version 2.4.0
  */
 
 'use strict';
@@ -107,10 +107,31 @@ Database.prototype.meta = function(name, value) {
 	return self;
 };
 
-Database.prototype.insert = function(doc) {
+Database.prototype.insert = function(doc, unique) {
 	var self = this;
-	var builder = new DatabaseBuilder2();
-	var json = doc.$clean ? doc.$clean() : doc;
+	var builder;
+
+	if (unique) {
+		builder = self.one();
+		var callback;
+
+		builder.callback(function(err, d) {
+			if (d)
+				callback && callback(null, 0);
+			else
+				self.insert(doc).callback(callback);
+		});
+
+		builder.callback = function(fn) {
+			callback = fn;
+			return builder;
+		};
+
+		return self;
+	}
+
+	builder = new DatabaseBuilder2();
+	var json = framework_builders.isSchema(doc) ? doc.$clean() : doc;
 	self.pending_append.push({ doc: JSON.stringify(json), builder: builder });
 	setImmediate(() => self.next(1));
 	self.emit('insert', json);
@@ -118,43 +139,7 @@ Database.prototype.insert = function(doc) {
 };
 
 Database.prototype.upsert = function(doc) {
-	var self = this;
-	var builder = self.one();
-	var callback;
-
-	builder.callback(function(err, d) {
-		if (d)
-			callback && callback(null, 0);
-		else
-			self.insert(doc).callback(callback);
-	});
-
-	builder.callback = function(fn) {
-		callback = fn;
-		return builder;
-	};
-
-	return builder;
-};
-
-Database.prototype.upsert = function(doc) {
-	var self = this;
-	var builder = self.find();
-	var callback;
-
-	builder.callback(function(err, d) {
-		if (d)
-			callback && callback(null, 0);
-		else
-			self.insert(doc).callback(callback);
-	});
-
-	builder.first().callback = function(fn) {
-		callback = fn;
-		return builder;
-	};
-
-	return builder;
+	return this.insert(doc, true);
 };
 
 Database.prototype.update = function(doc, insert) {
